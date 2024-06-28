cea77831ee1f66097775fcd569cfd3cd63ad3558
Animesh Shukla <animeshshukla1518@gmail.com>
2024-06-28 19:36:12 +0530

    feat: added code for compression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{env::args, fs::File, io::{BufReader,copy}, time::Instant};
+
+use flate2::{write::GzEncoder, Compression};
+
 fn main() {
-    
+    let n=args().len();
+    println!("No of arguments={}",n);
+    if n!=3{
+        println!("Source & Target are required");
+        return;
+    }
+    let mut input=BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output=File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder=GzEncoder::new(output, Compression::default());
+
+    let start=Instant::now();
+
+    copy(&mut input,&mut encoder).unwrap();
+    let output=encoder.finish().unwrap();
+
+    println!(
+        "Source len:{:?}",
+        input.get_ref().metadata().unwrap().len()
+    );
+
+    println!(
+        "Target len:{:?}",
+        output.metadata().unwrap().len()
+    );
+
+    println!(
+        "Time elapsed:{:?}",
+        start.elapsed()
+    );
 }
